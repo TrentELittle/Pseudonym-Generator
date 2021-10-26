e780db7c074a9463815a7f995ed692a476e21be3
Trent Little <trent.e.little@gmail.com>
2021-10-26 10:01:44 -0400

    Initialized project and created two initial lists

---
+++ main.py
@@ -0,0 +1,14 @@
+# The purpose of this project is to generate a random name based on two lists.
+
+# Decided to use tuples as container since updates to the lists wont be required
+
+# List of Colors and list of common house plant names
+colors=('Green', 'Blue', 'Yellow', 'Red', 'Orange', 'Purple',
+        'Brown', 'White', 'Black', 'Cyan', 'Magenta',
+        'Lilac', 'Lime', 'Pink')
+plant_names=('Fern', 'Evergreen', 'Monstera', 'Maranta', 'Pothos',
+             'Fig', 'Chestnut', 'Alocasia', 'Yucca', 'Perperomia',
+             'Lily', 'Aloe', 'Ivy', 'Anthurium', 'Calathea', 'Croton',
+             'Jade', 'Staghorn', 'Orchid', 'Begonia')
+
+
