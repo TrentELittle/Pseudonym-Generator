3d13b15839693e0c8c3fb3d0236f126ded346f16
Trent Little <trent.e.little@gmail.com>
2021-10-26 10:15:02 -0400

    rough draft of code

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # The purpose of this project is to generate a random name based on two lists.
+import sys, random
 
 # Decided to use tuples as container since updates to the lists wont be required
-
-# List of Colors and list of common house plant names
+# List of Colors and list of common house plant names to create a 'Plant Alias'
 colors=('Green', 'Blue', 'Yellow', 'Red', 'Orange', 'Purple',
         'Brown', 'White', 'Black', 'Cyan', 'Magenta',
         'Lilac', 'Lime', 'Pink')
@@ -11,4 +11,25 @@ plant_names=('Fern', 'Evergreen', 'Monstera', 'Maranta', 'Pothos',
              'Lily', 'Aloe', 'Ivy', 'Anthurium', 'Calathea', 'Croton',
              'Jade', 'Staghorn', 'Orchid', 'Begonia')
 
+
+# Start intro of application
+print('Welcome to the Plant Alias name generator!')
+# Start loop to randomly choose a color for a first name and a plant name for a last name
+
+while True:
+    first = random.choice(colors)
+    last = random.choice(plant_names)
+
+    print('\n\n')
+    print('An Alias is being created...')
+
+    print('\n\n')
+    print('Your new Alias is: {} {}'.format(first, last), file=sys.stderr)
+    print('\n\n')
+
+    try_again = input('Would you like a different Plant Alias? (y/n)')
+    if try_again.lower() == 'n':
+        break
+
+input('\nPress Enter to exit.')
 
